e8b5c0740e41bd787626433fcc91dc21f932c442
IkedaNoritaka <50833174+NoritakaIkeda@users.noreply.github.com>
2024-06-15 20:02:03 +0900

    fix: 個数の追加

--- src/feature/Header/Header.tsx
+++ src/feature/Header/Header.tsx
@@ -1,17 +1,20 @@
-import { Box, Link, HStack, Image, Heading } from "@chakra-ui/react";
+import { Box, Link, HStack, Image, Heading, Text } from "@chakra-ui/react";
 import hogeImage from "../../asset/pato_lamp_on_red.png";
 
 export const Header = () => {
   return (
     <>
-      <Link href={"/"} _hover={{ textDecoration: "none", color: "inherit" }}>
-        <HStack p={"20px"}>
-          <Image src={hogeImage} w={"40px"} />
-          <Heading fontSize={"25px"} color={"#222222"}>
-            パト猫ボタン
-          </Heading>
-        </HStack>
-      </Link>
+      <HStack p={"20px"} alignItems="flex-end">
+        <Link href={"/"} _hover={{ textDecoration: "none", color: "inherit" }}>
+          <HStack h={"30px"}>
+            <Image src={hogeImage} w={"30px"} />
+            <Heading fontSize={"25px"} color={"#222222"}>
+              パト猫ボタン
+            </Heading>
+          </HStack>
+        </Link>
+        <Text fontSize={"12px"}>現在100個</Text>
+      </HStack>
       <Box width="100%" height="1px" bg="gray.200" my={"4px"} />
     </>
   );
