6d0bef137003e5b92ed8face99ea256b133ba07e
IkedaNoritaka <50833174+NoritakaIkeda@users.noreply.github.com>
2024-06-17 01:27:24 +0900

    fix: シェアボタンの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import { Box } from "@chakra-ui/react";
-import { ContactUsButton, ContentTabs, Footer, Header } from "./feature";
+import { Box, HStack } from "@chakra-ui/react";
+import {
+  ContactUsButton,
+  ContentTabs,
+  Footer,
+  Header,
+  TweetButton,
+} from "./feature";
 
 function App() {
   return (
     <>
       <Header />
       <Box px={{ base: "5%", md: "10vw" }} py={"20px"}>
-        <Box py={"20px"}>
+        <HStack py={"20px"}>
           <ContactUsButton />
-        </Box>
+          <TweetButton />
+        </HStack>
         <ContentTabs />
       </Box>
       <Footer />
--- src/feature/index.ts
+++ src/feature/index.ts
@@ -2,6 +2,7 @@ export * from "./ContactUsButton";
 export * from "./ContentTabs";
 export * from "./Footer";
 export * from "./Header";
+export * from "./TweetButton";
 export * from "./VoiceButton";
 export * from "./VoiceButtonList";
 export * from "./YoutubeLinkList";
