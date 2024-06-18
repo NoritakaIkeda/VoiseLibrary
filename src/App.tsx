3fe70fbf96040c4c51cce368741870cea069b266
IkedaNoritaka <50833174+NoritakaIkeda@users.noreply.github.com>
2024-06-19 01:33:05 +0900

    fix: 問い合わせボタンのデザインと配置を調整

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
-import { Box, HStack } from "@chakra-ui/react";
-import {
-  ContactUsButton,
-  ContentTabs,
-  Footer,
-  Header,
-  TweetButton,
-} from "./feature";
+import { Box } from "@chakra-ui/react";
+import { ContentTabs, Footer, Header } from "./feature";
 
 function App() {
   return (
     <>
       <Header />
       <Box px={{ base: "5%", md: "10vw" }} py={"20px"}>
-        <HStack py={"20px"}>
-          <ContactUsButton />
-          <TweetButton />
-        </HStack>
         <ContentTabs />
       </Box>
       <Footer />
