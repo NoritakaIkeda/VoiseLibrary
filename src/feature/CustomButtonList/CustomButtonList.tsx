f81cf99a6bdbae3fbad4b2312e8512c26babda93
IkedaNoritaka <50833174+NoritakaIkeda@users.noreply.github.com>
2024-07-31 00:52:23 +0900

    fix: 選択を促す文言を追加

--- src/feature/CustomButtonList/CustomButtonList.tsx
+++ src/feature/CustomButtonList/CustomButtonList.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Image, Text } from "@chakra-ui/react";
 import { VoiceButton } from "../VoiceButton";
 import { useEffect, useState } from "react";
 
@@ -24,22 +24,46 @@ export const CustomButtonList = ({ isActive }: Props) => {
   }, [isActive]);
 
   if (storedItems.length === 0) {
-    return null;
+    return (
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        p="50px"
+      >
+        <Image
+          src={"/sleep_animal_cat.png"}
+          alt={"twitter"}
+          w={"300px"}
+          opacity={"80%"}
+        />
+        <Text textAlign="center" mt={2} color={"gray"}>
+          <Text as="span" fontWeight="bold">
+            ボイス一覧→会話デッキを編集
+          </Text>
+          から、自分だけの会話デッキを選んで設定することができます
+        </Text>
+      </Box>
+    );
   }
 
   return (
-    <Grid
-      templateColumns={{ base: "repeat(3, 1fr)", md: "repeat(7, 1fr)" }}
-      gap={6}
-    >
-      {storedItems.map((item, index) => (
-        <GridItem key={index}>
-          <VoiceButton
-            name={item.name}
-            audioResourcePath={item.audioResourcePath}
-          />
-        </GridItem>
-      ))}
-    </Grid>
+    <>
+      <></>
+      <Grid
+        templateColumns={{ base: "repeat(3, 1fr)", md: "repeat(7, 1fr)" }}
+        gap={6}
+      >
+        {storedItems.map((item, index) => (
+          <GridItem key={index}>
+            <VoiceButton
+              name={item.name}
+              audioResourcePath={item.audioResourcePath}
+            />
+          </GridItem>
+        ))}
+      </Grid>
+    </>
   );
 };
